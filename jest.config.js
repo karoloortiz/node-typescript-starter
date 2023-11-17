ede49a55d28f32f034bc8e2dceebffc79dd7c924
Karol Ortiz <zitrokarol@gmail.com>
2023-11-17 17:15:20 +0000

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
 module.exports = {
+  verbose: true,
   transform: {
-    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
   },
   testEnvironment: "node",
   testRegex: "./src/.*\\.(test|spec|steps)?\\.(ts|ts)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   roots: ["<rootDir>/src"],
   extensionsToTreatAsEsm: [".ts", ".tsx"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
